616f9632b2191b0060b9f63f069bff3d211d84ab
Mahfuz Ahmed <108610927+mahfuzahmed7@users.noreply.github.com>
2024-12-04 19:01:12 +0600

    added partialy event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('home added')
+//add money to the account
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    //prevent page reload after submit
+    event.preventDefault();
+    //get money to be added to the account balance
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+    //ge the pin number provided
+    const pinNumber = document.getElementById('input-pin-number').value;
+    console.log(pinNumber);
+});
+
