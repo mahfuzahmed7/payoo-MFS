3c7a4d2b5afc4881da80a72d49fb684739043265
Mahfuz Ahmed <108610927+mahfuzahmed7@users.noreply.github.com>
2024-12-04 18:29:34 +0600

    added event handler home

--- js/login.js
+++ js/login.js
@@ -1,6 +1,29 @@
+// document.getElementById('button-login').addEventListener('click', function (event) {
+//     event.preventDefault();
+
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber, pinNumber);
+
+//     if (phoneNumber === '5' && pinNumber === '1234') {
+//         console.log('you are login')
+//     }
+//     else {
+//         alert('wrong phone number or pin');
+//     }
+// })
+
 document.getElementById('button-login').addEventListener('click', function (event) {
     event.preventDefault();
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
-})
+    const pinNumber = document.getElementById('pin-number').value;
+
+    if (phoneNumber === '5' && pinNumber == '1234') {
+        console.log('you are login');
+        window.location.href = '/home.html';
+    }
+    else {
+        alert('wrong phone number or pin')
+    }
+});
